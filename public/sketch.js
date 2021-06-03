c372cf7b183fdb0473bfb271b153a4a643ef5abd
mensajito.mx <mensajito.mx@gmail.com>
2021-06-03 13:29:08 -0500

    Sesión 2

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,23 @@
+let socket;
+
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(500, 500);
+    socket = io.connect("http://localhost:3000");
+    background("#00FF00");
 }
 
 function draw() {
-  background("#FF0000");
-  noStroke();
-  fill("#0000FF");
-  ellipse(mouseX, mouseY, 50, 50);
+
+}
+
+function mouseDragged() {
+    let posicion = {
+        x: mouseX,
+        y: mouseY
+    }
+    noStroke();
+    fill("#FF0000");
+    ellipse(mouseX, mouseY, 50, 50);
+    console.log(mouseX, mouseY);
+    socket.emit('mouse', posicion);
 }
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 let express = require('express');
+let socket = require('socket.io');
 let app = express();
 let server = app.listen(3000);
+let io = socket(server);
 app.use(express.static('public'));
 
 console.log("Está funcionando el servidor");
+
+io.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log('Usuario Conectado: ' + socket.id);
+
+    socket.on('mouse', mouseMsg);
+}
+
+function mouseMsg(data) {
+    console.log(data);
+}
