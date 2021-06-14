21b4b73759d6105cb3d613ff5b1d61232a207706
mensajito.mx <mensajito.mx@gmail.com>
2021-06-14 12:19:11 -0500

    Cuarta Sesión

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 let express = require('express');
 let socket = require('socket.io');
 let app = express();
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 4000;
 let server = app.listen(port);
 let io = socket(server);
 app.use(express.static('public'));
 
-console.log("Está funcionando el servidor");
+console.log("Servidor Corriendo");
 
 io.on('connection', newConnection);
 
